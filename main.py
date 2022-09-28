f168b99ce848500b29fbcbd5835898390202b9f3
Nep2un <jupi2erinspace@gmail.com>
2022-09-28 12:25:31 -0700

    Finalize starting display for program.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-def main():
-    initialize()
-
 def initialize():
     print(r"""
-========================================================
-|                                                     |
-|    CCCCCCCCC   H     H      AA    TTTTTTTTTTTT      |
-|   C            H     H     A  A        TT           |
-|   C            HH H HH    A AA A       TT           |
-|   C            H     H   A      A      TT           |
-|    CCCCCCCCC   H     H  A        A     TT           |
-|                                                     |
-=======================================================""") 
-    # Text displaying CHAT IS online, aswell with help command to see all commands w/description
+==========================================================
+|                                                        |
+|       CCCCCCCCC   H     H      AA    TTTTTTTTTTTT      |
+|      C            H     H     A  A        TT           |
+|      C            HH H HH    A AA A       TT           |
+|      C            H     H   A      A      TT           |
+|       CCCCCCCCC   H     H  A        A     TT           |
+|                                                 online |
+==========================================================""")
+    print("Use 'help' command to view a list of commands w/ description.\n")
+    
+def main():
+    initialize()
 
 if __name__ == "__main__":
     main()
