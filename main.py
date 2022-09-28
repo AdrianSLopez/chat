8633631928c99d642273f72ab8fe2b491126a3cc
AdrianSLopez <41001.adrian@gmail.com>
2022-09-28 11:18:40 -0700

    Create main file

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    initialize()
+
+def initialize():
+    print(r"""
+========================================================
+|                                                     |
+|    CCCCCCCCC   H     H      AA    TTTTTTTTTTTT      |
+|   C            H     H     A  A        TT           |
+|   C            HH H HH    A AA A       TT           |
+|   C            H     H   A      A      TT           |
+|    CCCCCCCCC   H     H  A        A     TT           |
+|                                                     |
+=======================================================""") 
+    # Text displaying CHAT IS online, aswell with help command to see all commands w/description
+
+if __name__ == "__main__":
+    main()
